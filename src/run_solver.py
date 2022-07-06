f850dd3a58ce354562654c0db0cc22cfcbd38010
Lucas Godshalk <lucasmg91@gmail.com>
2022-07-06 10:28:18 -0400

    Nit: Fix case names

--- src/run_solver.py
+++ src/run_solver.py
@@ -2,25 +2,25 @@ from logic.powerflowsettings import PowerFlowSettings
 from logic.powerflow import PowerFlow
 
 # path to the grid network RAW file
-#casename = 'test/testcases/GS-4_prior_solution.RAW'
-#casename = 'testcases/IEEE-14_prior_solution.RAW'
-#casename = 'test/testcases/IEEE-118_prior_solution.RAW'
-#casename = 'test/testcases/ACTIVSg500_prior_solution_fixed.RAW'
-#casename = 'test/testcases/PEGASE-9241_flat_start.RAW'
-#casename = 'testcases/PEGASE-13659_flat_start.RAW'
-#casename = 'test/testcases/GS-4_stressed.RAW'
-#casename = 'testcases/IEEE-14_stressed_1.RAW'
-#casename = 'testcases/IEEE-14_stressed_2_fixed.RAW'
+#casename = 'test/data/positiveseq/GS-4_prior_solution.RAW'
+#casename = 'test/data/positiveseq/IEEE-14_prior_solution.RAW'
+#casename = 'test/data/positiveseq/IEEE-118_prior_solution.RAW'
+casename = 'test/data/positiveseq/ACTIVSg500_prior_solution_fixed.RAW'
+#casename = 'test/data/positiveseq/PEGASE-9241_flat_start.RAW'
+#casename = 'test/data/positiveseq/PEGASE-13659_flat_start.RAW'
+#casename = 'test/data/positiveseq/GS-4_stressed.RAW'
+#casename = 'test/data/positiveseq/IEEE-14_stressed_1.RAW'
+#casename = 'test/data/positiveseq/IEEE-14_stressed_2_fixed.RAW'
 #casename = 'test/data/gc_12_47_1/node.glm'
-casename = 'test/data/ieee_four_bus/node.glm'
+#casename = 'test/data/ieee_four_bus/node.glm'
 
 print("Running power flow solver...")
-print(f'Testcase: {casename.replace("testcases/", "")}')
+print(f'Testcase: {casename}')
 
-settings = PowerFlowSettings(debug=False, max_iters=50, flat_start=False, infeasibility_analysis=True, tx_stepping=False, voltage_limiting=False)
+settings = PowerFlowSettings(debug=False, max_iters=50, flat_start=False, infeasibility_analysis=False, tx_stepping=False, voltage_limiting=False)
 
 powerflow = PowerFlow(casename, settings)
 
 results = powerflow.execute()
 
-results.display(verbose=True)
+results.display(verbose=False)
