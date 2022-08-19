03ad657e742b9234ab0605dda71076a300452269
Lucas Godshalk <lucasmg91@gmail.com>
2022-08-19 14:20:00 -0400

    Add ability to download network file from url. Add check for if the network file exists on the local disk.

--- src/logic/networkloader.py
+++ src/logic/networkloader.py
@@ -1,26 +1,53 @@
-from itertools import count
 import os
-from typing import Tuple
 from logic.networkmodel import NetworkModel, TxNetworkModel
 from logic.parsers.raw.parser import parse_raw
 from logic.powerflowsettings import PowerFlowSettings
 from logic.parsers.threephase.threephaseparser import ThreePhaseParser
 from models.shared.L2infeasibility import L2InfeasibilityCurrent
+import urllib.request
+import tempfile
+
+def pull_network_file(network_uri: str):
+    #If it's a local file, we're all good.
+    if os.path.isfile(network_uri):
+        return network_uri
+    
+    #Otherwise, we attempt to download it as a url.
+    if not (network_uri.startswith("http://") or network_uri.startswith("https://")):
+        raise Exception("Unkonwn network file location")
+    
+    status_code = urllib.request.urlopen(network_uri).getcode()
+    if status_code != 200:
+        raise Exception("Network request for network file failed.")
+
+    tmpdir = tempfile.gettempdir()
+
+    filepath = os.path.join(tmpdir, network_uri.split("/")[-1]) #Assume the network file name is at the end of the url.
+
+    try:
+        os.remove(filepath)
+    except:
+        pass
+
+    filepath, _ = urllib.request.urlretrieve(network_uri, filepath)
+
+    return filepath
+
 
 class NetworkLoader:
     def __init__(self, settings: PowerFlowSettings):
         self.settings = settings
         
-    def from_file(self, network_file: str) -> NetworkModel:
-        if not os.path.isfile(network_file):
-            raise Exception("Network file does not exist.")
+    def from_file(self, network_uri: str) -> NetworkModel:
+        print(f"Loading network file: {network_uri}")
+        network_file = pull_network_file(network_uri)
 
         if ".glm" in network_file:
             return self.__create_three_phase_network(network_file)
         elif ".RAW" in network_file:
             return self.__create_positive_seq_network(network_file)
         else:
-            raise Exception("Invalid netlist file format")
+            raise Exception("Unknown network file format")
 
     def __create_three_phase_network(self, network_file: str):
         parser = ThreePhaseParser(network_file, self.settings)
